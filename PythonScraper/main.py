8ea8f2acdcb96cde23e3cfd3bcefeddc1909ad0b
Maarten <maartenvdh@live.be>
2020-09-26 13:53:55 +0200

    Removal of Gray area and Agree screen

--- PythonScraper/main.py
+++ PythonScraper/main.py
@@ -1,12 +1,13 @@
 from selenium import webdriver
 
-
 PATH = "C:/Program Files (x86)/chromedriver.exe"
 driver = webdriver.Chrome(PATH)
-
 
 google_url = "https://www.google.com/search?q=python" + "&num=" + str(5)
 driver.get(google_url)
 
-grayArea = driver.find_element_by_xpath('//div[@jsname="bF1uUb"]')
-driver.execute_script("document.querySelector('[jsname='bF1uUb']').style.display = 'none'")
+# Remove Agree button and grayed out background
+jsname = "bF1uUb"
+driver.execute_script("document.querySelector('[jsname=" + jsname + "]').style.display = 'none'")
+className = "bErdLd aID8W wwYr3"
+driver.execute_script("document.getElementsByClassName('" + className + "')[0].style.display = 'none'")
