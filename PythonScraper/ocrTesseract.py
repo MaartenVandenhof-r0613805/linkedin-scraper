c9323d813ed531f492e87a6b4a2b2339b27f68aa
Maarten <maartenvdh@live.be>
2020-09-30 12:15:00 +0200

    Text to PDF

--- PythonScraper/ocrTesseract.py
+++ PythonScraper/ocrTesseract.py
@@ -1,16 +1,22 @@
 from PIL import Image
-import os
+from fpdf import FPDF
 import pytesseract as pt
 import os
 
 pt.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 directory = "./screenshots"
 
-textFromScreenshots = []
+pdfFiles = []
+titleInt = 1
 for filename in os.listdir(directory):
     im = Image.open(directory + "/" + filename)
-    #pt.image_to_pdf_or_hocr(im, lang="nl")
-    text = pt.image_to_string(im, lang="nld")
-    print(text)
+    pdf = pt.image_to_pdf_or_hocr(im, lang="nld", extension="pdf")
+    
+    # Write to PDF
+    with open("./pdfFiles/pdf_" + str(titleInt) + ".pdf", "w+b") as f:
+        f.write(pdf)
+    f.close()
+    print("file " + str(titleInt) + " written")
+    titleInt = titleInt + 1
 
-
+    #text = pt.image_to_string(im, lang="nld")
